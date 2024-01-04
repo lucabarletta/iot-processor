b00097d32b2963963a453153385d12c50bb4e628
Luca Barletta <luca@barletta.ch>
2024-01-04 18:11:23 +0100

    implement concurrent hashmap

--- iot-processor/src/main/java/IoTp/actors/ManagerActor.java
+++ iot-processor/src/main/java/IoTp/actors/ManagerActor.java
@@ -8,29 +8,29 @@ import akka.actor.Props;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @ActorComponent
 public class ManagerActor extends AbstractActor {
     private static final Logger Log = LoggerFactory.getLogger(ManagerActor.class);
-    private final Map<String, ActorRef> processingActorRegistry = new HashMap<>();
+    private final Map<String, ActorRef> processingActorRegistry = new ConcurrentHashMap<>();
 
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(SensorData.class, data -> {
+                    // NOTE: important to only dequeue amount of message the actor system can handle
                     context().sender().tell("ack", self());
-                    var actorRef = processingActorRegistry.get(data.sensorId());
-                    if (actorRef != null) {
-                        actorRef.tell(data, context().self());
-                        Log.info("forward data to existing processing actor: " + actorRef.path().name());
-                    } else {
-                        final ActorRef child = getContext().actorOf(Props.create(ProcessingActor.class), data.sensorId());
-                        Log.info("add processing actor to registry: " + child.path().name());
-                        processingActorRegistry.put(data.sensorId(), child);
-                        child.tell(data, context().self());
-                    }
+                    processingActorRegistry.computeIfAbsent(data.sensorId(), id ->
+                            getContext().actorOf(Props.create(ProcessingActor.class), data.sensorId())).tell(data, ActorRef.noSender());
+//                    Log.info("forward data to existing processing actor: " + actorRef.path().name());
+//                    } else {
+//                        final ActorRef child = getContext().actorOf(Props.create(ProcessingActor.class), data.sensorId());
+//                        Log.info("add processing actor to registry: " + child.path().name());
+//                        processingActorRegistry.put(data.sensorId(), child);
+//                        child.tell(data, context().self());
+//                    }
                 })
                 .match(RemoveReferenceMessage.class, action -> {
                     processingActorRegistry.remove(action.ref.path().name(), action.ref);
